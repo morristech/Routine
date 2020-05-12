0392e87146f39f4bbb8dd60ac29934a21bf0be55
yasser <spookyframework@gmail.com>
2020-05-12 23:10:23 +0200

    feat(containers): Add Wrapper HOC

---
+++ client/containers/Wrapper.container.jsx
@@ -0,0 +1,33 @@
+/*
+ * This file is part of the Routine project.
+ *
+ * (c) Yasser Ameur El Idrissi <getspookydev@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+'use strict';
+
+import React, { Fragment } from 'react';
+import PropTypes from 'prop-types';
+import NavbarComponent from '../components/Navbar.component';
+import FooterComponent from '../components/Footer.component';
+
+function WrapperHocComponent(WrappedComponent) {
+  return (props) => (
+    <Fragment>
+      <NavbarComponent logoSrc={require('../logo.svg')} />
+      <div className="container mx-auto h-screen px-8 pt-4">
+        <WrappedComponent {...props} />
+      </div>
+      <FooterComponent />
+    </Fragment>
+  );
+}
+
+WrapperHocComponent.propTypes = {
+  WrappedComponent: PropTypes.element.isRequired,
+};
+
+export default WrapperHocComponent;
