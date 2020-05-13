1425833f71e40f23198afaa8b757ee7f36271dce
yasser <spookyframework@gmail.com>
2020-05-14 00:28:14 +0200

    style(components): Change Alert content & style

--- client/components/Alert.component.jsx
+++ client/components/Alert.component.jsx
@@ -14,26 +14,25 @@ import PropTypes from 'prop-types';
 import { withNamespaces } from 'react-i18next';
 
 function AlertComponent(props) {
+  /** @props **/
   const { t: lang } = props;
+
   return (
-    <div className="bg-indigo-900 text-center py-4 lg:px-4">
+    <div
+      className="text-center py-4 lg:px-4"
+      style={{ backgroundColor: '#231f20' }}
+    >
       <div
-        className="p-2 bg-indigo-800 items-center text-indigo-100 leading-none lg:rounded-full flex lg:inline-flex"
+        className="p-3 items-center leading-none lg:rounded-full flex lg:inline-flex"
         role="alert"
       >
-        <span className="flex rounded-full bg-indigo-500 uppercase px-2 py-1 text-xs font-bold mr-3">
-          {lang('Alert.component.badge')}
-        </span>
-        <span className="font-semibold mr-2 text-left flex-auto">
+        <span className="font-semibold pb-1 cursor-pointer text-gray-100 mr-2 underline text-2xl text-center flex-auto">
           {lang('Alert.component.content')}
+          <strong className="text-white">
+            {' ' + lang('Alert.component.github')}
+          </strong>
         </span>
-        <svg
-          className="fill-current opacity-75 h-4 w-4"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 20 20"
-        >
-          <path d="M12.95 10.707l.707-.707L8 4.343 6.586 5.757 10.828 10l-4.242 4.243L8 15.657l4.95-4.95z" />
-        </svg>
+        <i className="text-white text-4xl ml-2 fa fa-github" />
       </div>
     </div>
   );
