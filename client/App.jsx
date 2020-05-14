d499d9d0a06b85bf5bdccfa12fc204d16e0afbb8
yasser <spookyframework@gmail.com>
2020-05-14 22:50:06 +0200

    feat(src): Add App jsx file

---
+++ client/App.jsx
@@ -0,0 +1,26 @@
+/*
+ * This file is part of the Routine project.
+ *
+ * (c) Yasser Ameur El Idrissi <getspookydev@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+import React from 'react';
+import { Switch, Route, BrowserRouter } from 'react-router-dom';
+import { Routes } from './Routes';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        {Routes.map(({ id, path, component }) => {
+          return <Route key={id} exact path={path} component={component} />;
+        })}
+      </Switch>
+    </BrowserRouter>
+  );
+}
+
+export default App;
