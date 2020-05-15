ba75c11433780e0ea15fd7b852413912d100b5d3
yasser <spookyframework@gmail.com>
2020-05-15 23:49:20 +0200

    feat(daemon): Load given template

--- daemon/createRoutineApp.js
+++ daemon/createRoutineApp.js
@@ -9,29 +9,76 @@
 
 'use strict';
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const output = require('./output');
-const app = express();
-
-// parse application/json
-app.use(bodyParser);
-
-/**
- * The server object listens on port 4200.
- *
- * @var {number}
- */
-const $_PORT = process.env.PORT || 4200;
+var chalk = require('chalk'),
+  path = require('path'),
+  fs = require('fs'),
+  yaml = require('js-yaml'),
+  validateProjectName = require('validate-npm-package-name'),
+  child_process = require('child_process');
 
 /**
- * The HOST environment variable.
+ * Create Routine app.
  *
- * @var {string}
+ * @function
+ * @name createRoutineApp
+ * @param {string} appName
+ * @param {string} destinationPath
+ * @param {string} vcs
+ * @param {string} template
+ * @param {string} packageManager
+ * @returns {Promise<boolean>}
  */
-const $_HOST = process.env.HOST || '127.0.0.1';
+function createRoutineApp(
+  appName,
+  destinationPath,
+  template,
+  packageManager,
+  vcs = 'none'
+) {
+  return new Promise((resolve, reject) => {
+    if (typeof appName === 'undefined') {
+      console.log('For example:');
+      console.log(`${chalk.green('my-hamster-app')}`);
+      reject('Please specify the project name');
+    }
+    if (typeof template === 'undefined') {
+      console.log('For example:');
+      console.log(`${chalk.green('create-react-app')}`);
+      reject('Please specify the template');
+    }
+    if (typeof destinationPath === 'undefined') {
+      console.log('For example:');
+      console.log(`${chalk.green('C:\Users\User\Documents')}`);
+      reject('Please specify the project destination');
+    }
+    if (typeof packageManager === 'undefined') {
+      console.log('For example:');
+      console.log(`${chalk.green('yarn')}`);
+      reject('Please specify the package manager');
+    }
+    // validate app name
+    const validationResult = validateProjectName(appName);
+    if (!validationResult.validForNewPackages) {
+      reject(
+        `Cannot create a project named ${chalk.green(
+          `"${appName}"`,
+        )} because of npm naming restrictions:\n`,
+      );
+    }
+    console.log();
+    console.log(`${appName} accepted ${chalk.green('✓')}`);
+    const findTempltate = path.join(__dirname, '..', 'templates', template + '.yml');
+    // load template file example create-react-app.yml
+    let fileContents = fs.readFileSync(findTempltate, 'utf8');
+    // convert YAML file data to JS literals and objects.
+    let extractData = yaml.safeLoad(fileContents);
+    if (typeof extractData !== 'object')
+      reject('fileContents must be an object');
+    //
+    console.log();
+    console.log(`You are using ${extractData.name}`);
+    console.log(`You are using ${extractData.version}v`);
+    // checking template url.
 
-app.listen($_PORT, function (err) {
-  if (err) output.error(err);
-  output.appStarted($_PORT, $_HOST);
-});
+  });
+}
