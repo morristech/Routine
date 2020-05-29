45fa3935b179acafba5367d2a3df86b516a1d944
yasser <spookyframework@gmail.com>
2020-05-29 21:20:29 +0200

    feat(client): Import Alert component

--- client/containers/Wrapper.container.jsx
+++ client/containers/Wrapper.container.jsx
@@ -11,12 +11,14 @@
 
 import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
+import AlertComponent from '../components/Alert.component';
 import NavbarComponent from '../components/Navbar.component';
 import FooterComponent from '../components/Footer.component';
 
 function WrapperHocComponent(WrappedComponent) {
   return (props) => (
     <Fragment>
+      <AlertComponent />
       <NavbarComponent logoSrc={require('../icons/logo.svg')} />
       <div className="container mx-auto h-screen px-8 pt-4">
         <WrappedComponent {...props} />
